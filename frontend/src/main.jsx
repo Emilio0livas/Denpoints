d91facead25fcca27cea2a7287c473abb7172f97
joser <joserosasbarrios@gmail.com>
2026-06-12 14:35:32 -0700

    Configure Lab D frontend paths for jose

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/team-6-s26/lab-d-Emilio">
+    <BrowserRouter basename="/team-6-s26/lab-d-jrosas">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
